27f96476497ccfe5a840a8d9e51b4ad28ffcde68
NicoleCorina <nicolecorina.chaves@gmail.com>
2024-03-18 15:29:45 -0300

    mudei pra 100

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -2,7 +2,7 @@ let título = document.querySelector('h1');
 título.innerHTML = 'Jogo do Número Secreto';
 
 let parágrafo = document.querySelector('p');
-parágrafo.innerHTML = 'Escolha um número entre 1 e 10000000000';
+parágrafo.innerHTML = 'Escolha um número entre 1 e 100';
 
 function verificarChute() {
     console.log ('O botão foi clicado');
