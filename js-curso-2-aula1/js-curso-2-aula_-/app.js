cf6a9bbdb72742299793f7985cd0a7a521aff54a
NicoleCorina <nicolecorina.chaves@gmail.com>
2024-03-18 14:59:55 -0300

    estrutura inicial foi criada

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let título = document.querySelector('h1');
+título.innerHTML = 'Jogo do Número Secreto';
+
+let parágrafo = document.querySelector('p');
+parágrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log ('O botão foi clicado');
+}
