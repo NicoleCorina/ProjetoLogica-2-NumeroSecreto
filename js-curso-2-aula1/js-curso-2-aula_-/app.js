a9ee3c78eb2bf364893e985cd5fd865996f8adab
NicoleCorina <nicolecorina.chaves@gmail.com>
2024-04-01 16:06:23 -0300

    agrupando funções

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,9 +1,14 @@
-let título = document.querySelector('h1');
-título.innerHTML = 'Jogo do Número Secreto';
-
 let parágrafo = document.querySelector('p');
 parágrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 function verificarChute() {
-    console.log ('O botão foi clicado');
+    console.log ('O botão foi clicado!');
 }
